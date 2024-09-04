9597c6f4a6e781d21177fd644fb5fa49644f540b
sergey.shablenko <sergey.s@thoughtandfunction.com>
2024-09-04 22:15:13 +0300

    fix: more logs

--- android/src/main/java/com/carusto/ReactNativePjSip/NetworkChangeReceiver.java
+++ android/src/main/java/com/carusto/ReactNativePjSip/NetworkChangeReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Log;
 
 public class NetworkChangeReceiver extends BroadcastReceiver {
     private static final String TAG = "NetworkChangeReceiver";
