61eeaa58ec5f862c0d473671ea65d472dcb7a4cf
sergey.shablenko <sergey.s@thoughtandfunction.com>
2025-11-26 11:09:45 +0200

    feat: update pjsip binaries

--- libs.sh
+++ libs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="3.9"
+VERSION="4.0"
 URL="https://github.com/Wavecrest/react-native-pjsip-builder/releases/download/${VERSION}/release.tar.gz"
 LOCK=".libs.lock"
 DEST=".libs.tar.gz"
